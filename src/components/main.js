6c4026b5fad83ed36cba23c01789ff0351f51808
Bapun <bapunhansdah777@gmail.com>
2022-09-30 19:20:54 +0530

    graphql test

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import react from 'react'
+
+function Main(){
+  return (
+    <>
+       <div className="grid grid-cols-4 gap-10 bg-blue-500">
+        <div className="h-16 bg-red-300"></div>
+        <div className="h-16 bg-black"></div>
+        <div className="h-16 bg-black"></div>
+        <div className="h-16 bg-black"></div>
+      </div>
+    </>
+  )
+}
+
+export default Main;
