7a6979ee4a937e49fd0f73348998e1084a382382
Bapun <bapunhansdah777@gmail.com>
2022-10-04 09:18:14 +0530

    query test

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,35 @@
 import { ApolloServer,gql } from 'apollo-server';
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
-
+import {users,quotes} from './db.js'
 
 const typeDefs = gql`
       type Query{
-      	greet:String
+      	users:[User]
+      	user(_id:ID!):User
+      	quotes:[Quotes]
+      	s_quote(by:ID!):[Quotes]
+      }
+      type User{
+      	_id:ID!
+      	firstName:String
+      	lastName:String
+      	quotes:[Quotes]
+      }
+      type Quotes{
+      	by:ID
+      	quote:String
       }
 `
 
 const resolvers = {
 	Query:{
-		greet:()=>"hello world"
+		users:()=>users,
+		user:(_,{_id})=>users.find(user=>user._id == _id),
+		quotes:()=>quotes,
+		s_quote:(_,{by})=>quotes.filter(q=>q.by==by)
+	},
+	User:{
+            quotes:(ur)=>quotes.filter(q => q.by == ur._id)
 	}
 }
 
