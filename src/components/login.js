393452c928c58e08de4cdc7eaf2f1e8861007cff
Bapun <bapunhansdah777@gmail.com>
2022-10-13 18:21:18 +0530

    nothing

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,35 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import {useMutation,useQuery} from '@apollo/client'
+import {SIGN_IN} from '../gqloperations/mutation'
+import {useNavigate} from 'react-router-dom'
+import {GET_USER_DATA} from '../gqloperations/query'
+import { useApolloClient } from "@apollo/client";
 export default function Login(){
 
 	const [userSignInfo,setSignUserInfo] = useState({})
+	const navigate = useNavigate()
+    const client = useApolloClient();
+
+
+    const [signInUser,{data,error}] = useMutation(SIGN_IN,{
+    	variables:{
+    		 usersign:userSignInfo
+    	},
+    	onCompleted:(data)=>{
+    		localStorage.setItem("token",data.user.token)
+    		navigate("/")
+    	}
+    })
+
+
 
     function SignIn(e){
 		e.preventDefault()
 		console.log(userSignInfo)
+		signInUser()
 	}
+
 
 	function handleChange(e){
       setSignUserInfo({
@@ -23,7 +45,7 @@ export default function Login(){
              <input className="p-2" type="email" name="email" onChange={handleChange} placeholder="email" required/>
 		     <input className="p-2" type="password" name="password" onChange={handleChange} placeholder="password" required/>
 		     <input className="p-2 border bg-black text-white" type="submit"/>
-		     <div className="">New User ? <span className="text-blue-800"><Link to='register'>Register</Link></span></div>
+		     <div className="">New User ? <span className="text-blue-800"><Link to='/register'>Register</Link></span></div>
 		  </form>
 		  </div>
 		</div>
